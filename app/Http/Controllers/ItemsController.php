fb5c5afb5d28149419a0655a35820bf0a27d6cb6
programeri <programeri@ucione.local>
2017-05-05 19:20:48 +0200

    azurirano da novi item moze dodati samo registrirani korisnik

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -9,6 +9,16 @@ use App\Item;
 class ItemsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index','show']);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
